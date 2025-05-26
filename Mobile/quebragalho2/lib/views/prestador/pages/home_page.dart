3259766eb81ee2a76ef7808ea4d04855d4325c9a
Valhalla <valhallamaia@gmail.com>
2025-05-25 21:54:27 -0300

    acho que foi?

--- Mobile/quebragalho2/lib/views/prestador/pages/home_page.dart
+++ Mobile/quebragalho2/lib/views/prestador/pages/home_page.dart
@@ -2,27 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:quebragalho2/views/prestador/pages/detalhes_solicitacao.dart';
 import 'package:quebragalho2/views/prestador/widgets/solicitacao_cliente_card.dart';
 
-
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   final List<Map<String, String>> clientes = const [
-    {
-      'nome': 'João da Silva',
-      'foto': 'https://i.pravatar.cc/150?img=1',
-    },
-    {
-      'nome': 'Maria Oliveira',
-      'foto': 'https://i.pravatar.cc/150?img=2',
-    },
-    {
-      'nome': 'Carlos Pereira',
-      'foto': 'https://i.pravatar.cc/150?img=3',
-    },
-    {
-      'nome': 'Ana Souza',
-      'foto': 'https://i.pravatar.cc/150?img=4',
-    },
+    {'nome': 'João da Silva', 'foto': 'https://i.pravatar.cc/150?img=1'},
+    {'nome': 'Maria Oliveira', 'foto': 'https://i.pravatar.cc/150?img=2'},
+    {'nome': 'Carlos Pereira', 'foto': 'https://i.pravatar.cc/150?img=3'},
+    {'nome': 'Ana Souza', 'foto': 'https://i.pravatar.cc/150?img=4'},
     // Pode colocar quantos quiser, fera
   ];
 
@@ -38,20 +25,20 @@ class HomePage extends StatelessWidget {
             nome: cliente['nome']!,
             fotoUrl: cliente['foto']!,
             onTap: () {
-
               //Navegando para a página de detalhes da solicitação
               // Enviando os detalhes da solicitação para tela de solicitação
               Navigator.push(
                 context,
                 MaterialPageRoute(
-                  builder: (_) => DetalhesSolicitacaoPage(
-                    nome: cliente['nome']!,
-                    fotoUrl: cliente['foto']!,
-                    servico: 'Corte de Cabela',
-                    dataHora: '09/05/2025 das 14:00 às 15:00',
-                    valorTotal: 50,
-                  )
-                )
+                  builder:
+                      (_) => DetalhesSolicitacaoPage(
+                        nome: cliente['nome']!,
+                        fotoUrl: cliente['foto']!,
+                        servico: 'Corte de Cabela',
+                        dataHora: '09/05/2025 das 14:00 às 15:00',
+                        valorTotal: 50,
+                      ),
+                ),
               );
               // Aqui você pode adicionar a lógica para navegar para a página de detalhes do cliente
               // Navigator.push(context, MaterialPageRoute(builder: (context) => DetalhesClientePage(cliente: cliente)));
@@ -62,5 +49,3 @@ class HomePage extends StatelessWidget {
     );
   }
 }
-
-
